3c85fbdd3477e5e9075a7fd856af850c28d442f4
nitinkamra1992 <nitinkamra1992@gmail.com>
2016-04-20 21:19:33 -0700

    activations abstract class

--- serial/activations.h
+++ serial/activations.h
@@ -3,45 +3,87 @@
 
 #include <cmath>
 
-/*
- * Standard Activation Functions
- * Format Description:
- *		D(): Activation function derivative.
- *		F(): Activation function.
- *		operator(): Activation function.
- */
 namespace neuralnet{
+	
+	/*
+	 * Standard Activation Functions
+	 * Format Description:
+	 *		D(): Activation function derivative.
+	 *		F(): Activation function.
+	 *		operator(): Activation function.
+	 */
 
-	struct Sigmoid{
+	struct Activation
+	{
 		template<typename T>
-		inline T D(T x){
-			return F(x)*(1-F(x);
+		virtual inline T D(T x) = 0;
+
+		template<typename T>
+		virtual inline T F(T x) = 0;
+
+		template<typename T>
+		virtual inline T operator()(T x) = 0;
+	};
+
+	struct Sigmoid: public Activation
+	{
+		template<typename T>
+		inline T D(T x)
+		{
+			return F(x)*(1-F(x));
 		}
 
 		template<typename T>
-		inline T F(T x){
+		inline T F(T x)
+		{
 			return 1/(1 + exp(-x));
 		}
 
 		template<typename T>
-		inline T operator()(T x){
+		inline T operator()(T x)
+		{
 			return F(x);
 		}
 	};
 
-	struct FSigmoid{
+	struct FSigmoid: public Activation
+	{
 		template<typename T>
-		inline T D(T x){
+		inline T D(T x)
+		{
 				return 1.0/pow(1.0 + std::abs(x),2);
 		}
 
 		template<typename T>
-		inline T F(T x){
+		inline T F(T x)
+		{
 				return x/(1.0 + std::abs(x));
 		}
 
 		template<typename T>
-		inline T operator()(T x){
+		inline T operator()(T x)
+		{
+			return F(x);
+		}
+	};
+
+	struct ReLu: public Activation
+	{
+		template<typename T>
+		inline T D(T x)
+		{
+			return (x >= 0.0 ? 1.0 : 0.0);
+		}
+
+		template<typename T>
+		inline T F(T x)
+		{
+			return (x >= 0.0 ? x : 0.0);
+		}
+
+		template<typename T>
+		inline T operator()(T x)
+		{
 			return F(x);
 		}
 	};
