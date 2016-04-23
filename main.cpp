dab9a76fb5779e7f3b3ffadd5171954fc5b8acdb
vzois <vzois@usc.edu>
2016-04-23 15:58:01 -0700

    added gpu transpose kernel

--- main.cpp
+++ main.cpp
@@ -1,38 +1,9 @@
 #include"common/ArgParser.h"
 #include"common/Utils.h"
 
-#include"serial/SNeuralNet.h"
 #include"parallel_gpu/GNNConfig.h"
 
 #include<iostream>
-
-void example_layer_initialization(){
-	stdaf::Sigmoid s;//sigmoid activation function
-	stdaf::FSigmoid fs;// fast approximate sigmoid function
-
-	Layer<float,stdaf::Sigmoid> ls(2,3,s);//template float values, sigmoid function
-	// arguments 2 input neurons , 3 output neurons, sigmoid function definition
-
-
-	Layer<double,stdaf::FSigmoid> lfs(4,2,fs);//template double values, fast  sigmoid function
-		// arguments 4 input neurons , 2 output neurons, fast sigmoid function definition
-
-
-	std::cout<< "sigmoid activation function test: "<<ls(1.2) << std::endl;
-	std::cout<< "fast sigmoid activation function test: "<<lfs(1.2) << std::endl;
-
-	/*Example neural network construction*/
-	std::vector<Layer<float,stdaf::Sigmoid>*> nn;
-
-	nn.push_back(new Layer<float,stdaf::Sigmoid>(2,3,stdaf::Sigmoid()));/*2 inputs 3 nodes in first hidden layer*/
-	nn.push_back(new Layer<float,stdaf::Sigmoid>(2,3,stdaf::Sigmoid()));/*3 nodes prev hidden layer, 4 nodes next hidden layer */
-	nn.push_back(new Layer<float,stdaf::Sigmoid>(4,1,stdaf::Sigmoid()));/*4 nodes prev hidden layer, 1 output node*/
-
-	/*Testing activation function*/
-	std::cout<< "First layer: " << nn[0]->F(1.2) <<std::endl;
-	std::cout<< "Second layer: " << nn[1]->F(3.1) << std::endl;
-	std::cout<< "Output layer: " << nn[2]->F(0.5) << std::endl;
-}
 
 void example_gpu_bench_act(){
 	gnn::Sigmoid gs;
@@ -49,25 +20,30 @@ void example_gpu_bench_act(){
 	at.bench_act();
 }
 
-void example_gpu_initializing_weights(){
+void example_gpu_initializing_weights(ArgParser ap){
 	gnn::Sigmoid gs;
 	gnn::GNeuralNetwork<float,gnn::Sigmoid> s(gs);
-	s.loadExamplesFromFile("data/test.csv");
+	if(!ap.exists(FIARG)) vz::error("Please provide input file!!!");
+	s.loadExamplesFromFile(ap.getString(FIARG));
 	std::vector<int> layers;
 
 	layers.push_back(8); //INPUT
-	layers.push_back(4); //HIDDEN 1
-	layers.push_back(6); //HIDDEN 2
-	layers.push_back(2); //HIDDEN 3
-	layers.push_back(3); //OUTPUT
+	layers.push_back(16); //HIDDEN 1
+	layers.push_back(12); //HIDDEN 2
+	layers.push_back(14); //HIDDEN 3
+	layers.push_back(4); //OUTPUT
 
+	s.setBatchSize(8);
 	s.createLayers(layers);
-	s.print_weights();
+	//s.print_weights();
+
+	s.train();
 }
 
-int main(){
-	example_layer_initialization(); std::cout<<"<----------------------------------->" << std::endl;
+int main(int argc, char **argv){
+	ArgParser ap;
+	ap.parseArgs(argc,argv);
 	example_gpu_bench_act(); std::cout<<"<----------------------------------->" << std::endl;
-	example_gpu_initializing_weights();
+	example_gpu_initializing_weights(ap);
 
 }
--- parallel_gpu/GNNtrain.cu
+++ parallel_gpu/GNNtrain.cu
@@ -2,6 +2,10 @@
 #include "../common/Time.h"
 
 namespace gnn_kernels{
+
+	/*
+	 * Testing activation functions on kernels.
+	 */
 	template<typename ACT_F>
 	__global__ void bench_test_activation(ACT_F F){
 		int i = threadIdx.x + blockIdx.x * blockDim.x;
@@ -11,13 +15,90 @@ namespace gnn_kernels{
 		}
 	}
 
+	/*
+	 * Initialize matrices random weights
+	 */
 	template<typename DATA_T>
 	__global__ void randomWeights(DATA_T *W_j,unsigned int clayer, unsigned int nlayer){
 		int i = blockIdx.x * blockDim.x + threadIdx.x;
 
 		if( i < clayer * nlayer){
-			W_j[i] = i;
+			W_j[i] = cudaUniRand(i);
+			//W_j[i] =i;
 		}
+	}
+	/*
+	 * Initialize processing batch
+	 */
+	template<typename DATA_T,unsigned int TILE>
+	__global__ void loadTranspose(DATA_T *A_j, DATA_T *tEx,
+			unsigned int clayer, unsigned int bsize, unsigned int offset){
+		__shared__ DATA_T sAj[TILE*TILE];
+		int by = blockIdx.y, bx = blockIdx.x;
+		int ty = threadIdx.y, tx = threadIdx.x;
+		int boffset = (by * clayer + bx ) * TILE;
+
+		sAj[ty*TILE + tx] = tEx[boffset + offset + ty * clayer + tx];
+		__syncthreads();
+		boffset = (bx * bsize + by ) * TILE;
+		A_j[boffset + ty * bsize + tx] = sAj[tx * TILE + ty];
+
+		if( tx == 0  && ty == 0 && bx == 0 && by == 0){
+			//printf("%d>\n",tx);
+			for(int  k= 0;k <TILE * TILE;k++){
+			//	printf("%f ",sAj[k]);
+			//	if(((k+1) % TILE) == 0) printf("\n");
+			}
+		//	printf("<<>>><\n",tx);
+		}
+		__syncthreads();
+	}
+
+	/*
+	 * Compute matrix of activation values for a single layer of a given batch.
+	 *		1:	Current layer weight matrix.
+	 *		2: 	Current layer matrix of activation vectors.
+	 *		3: 	Next layer matrix of activation vectors.
+	 *		4:	W_j = nlayer x clayer , A_j = clayer x bsize, A_jj = nlayer x bsize.
+	 *		5: 	Offset: 0 for hidden and output layer, corresponding row of training example matrix for input layer.
+	 */
+	template<typename DATA_T, typename ACT_F, unsigned int TILE>
+	__global__ void batchActivation(
+			DATA_T *W_j, DATA_T *A_j, DATA_T *A_jj, ACT_F F,
+			unsigned int clayer, unsigned int nlayer, unsigned int bsize){
+		__shared__ DATA_T sWj[TILE][TILE];
+		__shared__ DATA_T sAj[TILE][TILE];
+
+		int by = blockIdx.y;
+		int bx = blockIdx.x;
+
+		int ty = threadIdx.y;
+		int tx = threadIdx.x;
+
+		int row = by * blockDim.y + ty;
+		int col = bx * blockDim.x + tx;
+		DATA_T Ajj = 0;
+
+		for(int t = 0; t < TILE ; t++){
+			sWj[ty][tx] = W_j[row*clayer + t*TILE + tx];
+			sAj[ty][tx] = A_j[(t*TILE + ty)*bsize + col];
+			__syncthreads();
+
+			for(int i = 0; i<TILE; i++){
+				Ajj += sWj[ty][i] * sAj[i][tx];
+			}
+			__syncthreads();
+		}
+		Ajj[row*bsize + col] = F.F(Ajj);
+	}
+
+	template<typename DATA_T>
+	__global__ void printGPU(DATA_T *A, unsigned int row, unsigned int col){
+		for(int i =0;i<row*col;i++){
+			printf("%f ", A[i]);
+			if((i+1)% col == 0) printf("\n");
+		}
+
 	}
 
 }
@@ -26,11 +107,13 @@ namespace gnn{
 
 	template<typename DATA_T, typename ACT_F>
 	void GNeuralNetwork<DATA_T,ACT_F>::randomInit(){
-		std::cout<<"Initializing random weights: "<<std::endl;
+		if(network == NULL) vz::error("Network architecture missing. Use createLayers first!");
+		//std::cout<<"Initializing random weights: "<<std::endl;
 		cudaSetDevice(0);
 
+		cudaInitRandStates();
 		for(int i = 0;i < layers-1;i++){
-			std::cout<<network[i].clayer << "-->" << network[i].nlayer << std::endl;
+			//std::cout<<network[i].clayer << "-->" << network[i].nlayer << std::endl;
 			unsigned int vector_size = network[i].nlayer * network[i].clayer;
 			dim3 grid = grid_1D(vector_size,256);
 			dim3 block = block_1D(256);
@@ -38,10 +121,62 @@ namespace gnn{
 		}
 	}
 
+	template<typename DATA_T, typename ACT_F>
+	void GNeuralNetwork<DATA_T,ACT_F>::train(){
+		if(network == NULL) vz::error("Network architecture missing. Use createLayers first!");
+		if(bsize == 0) vz::error("Batch size not set. Use setBatchSize first!");
+
+		std::cout<< "Start Training"<< std::endl;
+		unsigned int nbatch = dimEx.second / this->bsize; std::cout<< "Batch num: " << nbatch << std::endl;
+		createLayerBatch();
+
+		//for(int i = 0;i < nbatch;i++){
+		for(int i = 0;i < 1;i++){
+			LayerBatch<DATA_T> flayer = lbatch[0];
+			std::cout<<flayer.clayer << " x " << flayer.bsize << std::endl;
+
+			unsigned int TILE = 4;
+			unsigned int bRow = i * this->bsize * flayer.clayer ;
+
+			dim3 grid((flayer.clayer-1)/TILE + 1,(flayer.bsize - 1)/TILE + 1,1);
+			dim3 block(TILE,TILE,1);
+			print_grid(grid,block);
+			/*
+			 * Load transpose of train examples. Index should be zero
+			 */
+			if(TILE == 2)
+				gnn_kernels::loadTranspose<DATA_T,2><<<grid,block>>>
+				(flayer.A_j,examples,flayer.clayer,flayer.bsize,bRow);
+			else if(TILE == 4)
+				gnn_kernels::loadTranspose<DATA_T,4><<<grid,block>>>
+				(flayer.A_j,examples,flayer.clayer,flayer.bsize,bRow);
+			else if(TILE == 8)
+				gnn_kernels::loadTranspose<DATA_T,8><<<grid,block>>>
+				(flayer.A_j,examples,flayer.clayer,flayer.bsize,bRow);
+			handleDeviceErrors(cudaDeviceSynchronize(),"Error executing load transpose");
+
+			//printDevData<DATA_T>(examples,lbatch[i].bsize,lbatch[i].clayer);
+			//printDevData<DATA_T>(flayer.A_j,flayer.bsize,flayer.clayer);
+			gnn_kernels::printGPU<DATA_T><<<1,1>>>(flayer.A_j,flayer.clayer,flayer.bsize);
+		}
+
+
+		/*gnn_kernels::batchActivation<DATA_T,ACT_F><<<grid,block>>>(
+				network[l].W_j,
+				network[l].W_j,
+				lbatch[l].A_j,
+				F,
+				network[l].clayer,
+				network[l].nlayer,
+				lbatch[l].bsize,
+				0
+		);*/
+
+	}
+
 	/*
 	 * Testing methods
 	 */
-
 	template<typename DATA_T, typename ACT_F>
 	void GNeuralNetwork<DATA_T,ACT_F>::bench_act(){
 		cudaSetDevice(0);
@@ -69,10 +204,16 @@ namespace gnn{
 		DATA_T *cW_j;
 		cudaSetDevice(0);
 
-		for(int i = 0;i < layers-1;i++){
+		for(int i = 0;i < 1;i++){
 			unsigned int size = network[i].nlayer * network[i].clayer;
 			allocHostMem<DATA_T>(&cW_j,sizeof(DATA_T)*size, "Error Allocating Host Weight Matrix");
 			safeCpyToHost<DATA_T>(cW_j,network[i].W_j,sizeof(DATA_T)*size, "Error Allocating Copying Weight Matrix From Device");
+
+			for(int j = 0;j<size;j++){
+				std::cout<<cW_j[j] << " ";
+				if((j+1)%network[i].clayer == 0) std::cout<<std::endl;
+			}
+			std::cout<<std::endl;
 		}
 
 	}
